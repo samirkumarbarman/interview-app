83d113cf12f1c0a79058971f1e5ac23904b32380
Your Name <samirkumar2287@gmail.com>
2025-03-27 12:06:17 +0600

    Adding socket.io server in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -11,11 +11,20 @@ import writtenRoutes from './routes/writtenRoutes.js';
 import connectDB from './config/db.js';
 import errorHandler from './middlewares/errorHandler.js';
 import { limiter } from './middlewares/rateLimit.js';
+import { Server } from "socket.io";
+import http from "http";
+import { saveNewMessage } from "../src/services/chatServices.js";
+
 
 dotenv.config();
+
 connectDB();
 
 const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server);
 
 app.use(express.json());
 
@@ -37,6 +46,19 @@ app.use('/api/voices', voiceRoutes);
 
 app.use('/api/writtens', writtenRoutes);
 
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('chat message', async({sender, senderModel, receiver, receiverModel, message})=>{
+        const chat = await saveNewMessage(sender, senderModel, receiver, receiverModel, message);
+
+        socket.to(receiver).emit('chat messsage', chat);
+    });
+    socket.on('disconnect', ()=>{
+        console.log('User disconnected');
+    });
+});
+
 app.use(errorHandler);
+
 
 export default app;
